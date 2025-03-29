982241c3839dc4215482a5885c3b136568f7444b
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2025-03-29 21:46:10 +0700

    working song

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 $(document).ready(function () {
-  // Mainkan audio saat halaman dimuat
-  const audio = document.getElementById('birthdayAudio');
-  audio.play().catch((error) => {
-      console.log('Autoplay prevented:', error);
-  });
-
-  // Fungsi untuk buka kartu
-  function openCard() {
-      $('.card').stop().animate({
-          top: '-90px'
-      }, 'slow');
-  }
-
-  // Fungsi untuk tutup kartu
-  function closeCard() {
-      $('.card').stop().animate({
-          top: '0'
-      }, 'slow');
-  }
-
-  // Untuk desktop (hover)
-  $('.container').mouseenter(openCard).mouseleave(closeCard);
-
-  // Untuk mobile (click)
-  $('.container').on('click', function () {
-      if ($('.card').css('top') === '0px') {
-          openCard();
-      } else {
-          closeCard();
-      }
-  });
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
 });
