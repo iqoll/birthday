b95232b92dbc23a4b4e010176da51c6bc920f6cc
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2025-03-29 20:57:38 +0700

    song and mobile

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 $(document).ready(function () {
-  $('.container').mouseenter(function () {
+  // Mainkan audio saat halaman dimuat
+  const audio = document.getElementById('birthdayAudio');
+  audio.play().catch((error) => {
+      console.log('Autoplay prevented:', error);
+  });
+
+  // Fungsi untuk buka kartu
+  function openCard() {
       $('.card').stop().animate({
           top: '-90px'
       }, 'slow');
-  }).mouseleave(function () {
+  }
+
+  // Fungsi untuk tutup kartu
+  function closeCard() {
       $('.card').stop().animate({
-          top: 0
+          top: '0'
       }, 'slow');
+  }
+
+  // Untuk desktop (hover)
+  $('.container').mouseenter(openCard).mouseleave(closeCard);
+
+  // Untuk mobile (click)
+  $('.container').on('click', function () {
+      if ($('.card').css('top') === '0px') {
+          openCard();
+      } else {
+          closeCard();
+      }
   });
 });
